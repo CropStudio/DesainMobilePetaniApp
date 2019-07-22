8b5d6cb8eaca21cd45a3aed750961ecbbf969624
dwiramadhan <dwi.13421054@student.ubl.ac.id>
2019-07-22 23:58:43 +0700

    update UI dashboard

--- app/src/main/java/com/example/app4g/petani/Dashboard.java
+++ app/src/main/java/com/example/app4g/petani/Dashboard.java
@@ -1,9 +1,11 @@
 package com.example.app4g.petani;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -12,6 +14,8 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.LinearLayout;
 
 import com.example.app4g.R;
@@ -27,7 +31,20 @@ public class Dashboard extends Fragment {
     public SharedPreferences prefs;
     public SessionManager session;
 
+    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
 
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.fabs)
+    FloatingActionButton fabs;
+    @BindView(R.id.linearFab1)
+    LinearLayout linearFab1;
+    @BindView(R.id.linearFab2)
+    LinearLayout linearFab2;
+
+    private Boolean isFabOpen = false;
+
+    @SuppressLint("RestrictedApi")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -37,11 +54,44 @@ public class Dashboard extends Fragment {
         prefs = getActivity().getSharedPreferences("UserDetails",
                 Context.MODE_PRIVATE);
 
+        fab_open = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_open);
+        fab_close = AnimationUtils.loadAnimation(getActivity(),R.anim.fab_close);
+        rotate_forward = AnimationUtils.loadAnimation(getActivity(),R.anim.rotate_forward);
+        rotate_backward = AnimationUtils.loadAnimation(getActivity(),R.anim.rotate_backward);
         return view;
     }
 
-    @OnClick(R.id.linearExit)
-    void keluarApps(){
+//    @OnClick(R.id.linearExit)
+//    void keluarApps(){
+//        final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+//        builder.setTitle(Html.fromHtml("<font color='#009688'><b>Yakin ingin keluar ?</b></font>"))
+//                .setCancelable(false)
+//                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+//                    public void onClick(DialogInterface dialog, int id) {
+////                        keluar();
+//                        session.setLogin(false);
+//                        session.setSkip(false);
+//                        session.setSessid(0);
+//                        Intent intent = new Intent(getActivity(), Login.class);
+//                        startActivity(intent);
+//                        getActivity().finish();
+//                    }
+//                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                builder.setCancelable(true);
+//            }
+//        })
+//                .show();
+//    }
+
+    @OnClick(R.id.fab)
+    void openFab(){
+        animateFAB();
+    }
+
+    @OnClick(R.id.linearFab1)
+    void keluar(){
         final AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(Html.fromHtml("<font color='#009688'><b>Yakin ingin keluar ?</b></font>"))
                 .setCancelable(false)
@@ -56,12 +106,45 @@ public class Dashboard extends Fragment {
                         getActivity().finish();
                     }
                 }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @SuppressLint("RestrictedApi")
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 builder.setCancelable(true);
+                fab.setVisibility(View.VISIBLE);
+                fabs.setVisibility(View.GONE);
+                fab.startAnimation(rotate_backward);
+                linearFab1.startAnimation(fab_close);
+                linearFab2.startAnimation(fab_close);
+                linearFab1.setClickable(false);
+                linearFab2.setClickable(false);
+                isFabOpen = false;
             }
         })
                 .show();
+    }
+
+    @SuppressLint("RestrictedApi")
+    public void animateFAB(){
+
+        if(isFabOpen){
+            fab.setVisibility(View.VISIBLE);
+            fabs.setVisibility(View.GONE);
+            fab.startAnimation(rotate_backward);
+            linearFab1.startAnimation(fab_close);
+            linearFab2.startAnimation(fab_close);
+            linearFab1.setClickable(false);
+            linearFab2.setClickable(false);
+            isFabOpen = false;
+        } else {
+            fab.setVisibility(View.GONE);
+            fabs.setVisibility(View.VISIBLE);
+            fab.startAnimation(rotate_forward);
+            linearFab1.startAnimation(fab_open);
+            linearFab2.startAnimation(fab_open);
+            linearFab1.setClickable(true);
+            linearFab2.setClickable(true);
+            isFabOpen = true;
+        }
     }
 
     @Override
